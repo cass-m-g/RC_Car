5c409b1c46d6c7620a9fa4d6cbedf1e9f4faa463
cass-m-g <cass.m.garner@gmail.com>
2014-08-23 01:55:53 -0700

    Create binary_to_hex.h

---
+++ binary_to_hex.h
@@ -0,0 +1,38 @@
+#ifndef __binary_to_hex_h__
+#define __binary_to_hex_h__
+
+/* binbits.h - Represent char type by binary bits */
+#define b00000	0x00
+#define b00001	0x01
+#define b00010	0x02
+#define b00011	0x03
+#define b00100	0x04
+#define b00101	0x05
+#define b00110	0x06
+#define b00111	0x07
+#define b01000	0x08
+#define b01001	0x09
+#define b01010	0x0a
+#define b01011	0x0b
+#define b01100	0x0c
+#define b01101	0x0d
+#define b01110	0x0e
+#define b01111	0x0f
+#define b10000	0x10
+#define b10001	0x11
+#define b10010	0x12
+#define b10011	0x13
+#define b10100	0x14
+#define b10101	0x15
+#define b10110	0x16
+#define b10111	0x17
+#define b11000	0x18
+#define b11001	0x19
+#define b11010	0x1a
+#define b11011	0x1b
+#define b11100	0x1c
+#define b11101	0x1d
+#define b11110	0x1e
+#define b11111	0x1f
+
+#endif
