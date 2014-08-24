8f73eb2885c19fddad2525daf24034b93d50c28a
cass-m-g <cass.m.garner@gmail.com>
2014-08-24 00:04:52 -0700

    Update movement_controls.h

--- car_controls/movement_controls.h
+++ car_controls/movement_controls.h
@@ -1,6 +1,49 @@
 #ifndef __movement_controls_h__
 #define __movement_controls_h__
 
+//set max car speed***********
+double max_car_speed = 145;
+
+
+void set_car_speed(){
+	switch(button_press){
+		case '0':
+			
+			break;
+		case '1':
+			
+			break;
+		case '2':
+			
+			break;
+		case '3':
+			
+			break;
+		case '4':
+			
+			break;
+		case '5':
+			
+			break;
+		case '6':
+			
+			break;
+		case '7':
+			
+			break;
+		case '8':
+			
+			break;
+		case '9':
+			
+			break;
+		case 'q':
+			
+			break;
+	}
+}
+
+
 void set_car_movement(){
 	switch(current_car_state){
 		case 'F':
