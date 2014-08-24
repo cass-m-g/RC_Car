c68a0019e01a448e065e428588ebfa24ffd6ef75
cass-m-g <cass.m.garner@gmail.com>
2014-08-23 22:44:44 -0700

    Update global_vars.h

--- car_controls/global_vars.h
+++ car_controls/global_vars.h
@@ -1,6 +1,9 @@
 #ifndef __global_vars_h__
 #define __global_vars_h__
 
+
+//must include this file in the main before all other files
+
 extern unsigned char button_press;
 unsigned char look_direction;
 unsigned char eye_location;
