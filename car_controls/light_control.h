9d11f75d877cc02bc91612842da0a07eff1237b8
cass-m-g <cass.m.garner@gmail.com>
2014-08-26 14:38:24 -0700

    Update light_control.h

--- car_controls/light_control.h
+++ car_controls/light_control.h
@@ -14,14 +14,16 @@ int front_lights_Tck(int state){
 		case fl_off:
 		if(button_press == 'W'){
 			//turn lights on ********
-			PORTB = SetBit(PORTB, 1, 1);
+			PORTA = SetBit(PORTA, 0, 1);
+			PORTA = SetBit(PORTA, 1, 1);
 			state = fl_on;
 		}
 		break;
 		case fl_on:
 		if(button_press == 'w'){
 			//turn lights off *********
-			PORTB = SetBit(PORTB, 1, 0);
+			PORTA = SetBit(PORTA, 0, 0);
+			PORTA = SetBit(PORTA, 1, 1);
 			state = fl_off;
 		}
 		break;
@@ -43,14 +45,14 @@ int back_lights_Tck(int state){
 		case bl_off:
 		if(button_press == 'U'){
 			//turn lights on ********
-			PORTB = SetBit(PORTB, 2, 1);
+			//PORTB = SetBit(PORTB, 2, 1);
 			state = bl_on;
 		}
 		break;
 		case bl_on:
 		if(button_press == 'u'){
 			//turn lights off *********
-			PORTB = SetBit(PORTB, 2, 0);
+			//PORTB = SetBit(PORTB, 2, 0);
 			state = bl_off;
 		}
 		break;
