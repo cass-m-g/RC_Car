2a4fad0304e3790f30d2c92eb4b1f81039ef72a5
cass-m-g <cass.m.garner@gmail.com>
2014-08-26 01:13:51 -0700

    Update movement_controls.h

--- car_controls/movement_controls.h
+++ car_controls/movement_controls.h
@@ -1,6 +1,10 @@
 #ifndef __movement_controls_h__
 #define __movement_controls_h__
 
+//for now left wheel B0 = forward B1 = back
+// right B2 = forward b3 = back
+
+
 //set max car speed***********
 double max_car_speed = 145;
 
@@ -44,34 +48,80 @@ void set_car_speed(){
 }
 
 
+void set_car_stop(){
+	
+	PORTB = SetBit(PORTB, 0, 0);
+	PORTB = SetBit(PORTB, 1, 0);
+	PORTB = SetBit(PORTB, 2, 0);
+	PORTB = SetBit(PORTB, 3, 0);
+}
+
+
 void set_car_movement(){
 	switch(current_car_state){
 		case 'F':
 			//forward
+			PORTB = SetBit(PORTB, 0, 1);
+			PORTB = SetBit(PORTB, 1, 0);
+			PORTB = SetBit(PORTB, 2, 1);
+			PORTB = SetBit(PORTB, 3, 0);
+			
 			break;
 		case 'B':
+			PORTB = SetBit(PORTB, 0, 0);
+			PORTB = SetBit(PORTB, 1, 1);
+			PORTB = SetBit(PORTB, 2, 0);
+			PORTB = SetBit(PORTB, 3, 1);
 			//backward
 			break;
 		case 'L':
+			PORTB = SetBit(PORTB, 0, 0);
+			PORTB = SetBit(PORTB, 1, 1);
+			PORTB = SetBit(PORTB, 2, 1);
+			PORTB = SetBit(PORTB, 3, 0);
 			//left
 			break;
 		case 'G':
+			PORTB = SetBit(PORTB, 0, 0);
+			PORTB = SetBit(PORTB, 1, 1);
+			PORTB = SetBit(PORTB, 2, 1);
+			PORTB = SetBit(PORTB, 3, 0);
 			//forward left
 			break;
 		case 'H':
+			PORTB = SetBit(PORTB, 0, 1);
+			PORTB = SetBit(PORTB, 1, 0);
+			PORTB = SetBit(PORTB, 2, 0);
+			PORTB = SetBit(PORTB, 3, 1);
 			//back left
 			break;
 		case 'R':
+			PORTB = SetBit(PORTB, 0, 1);
+			PORTB = SetBit(PORTB, 1, 0);
+			PORTB = SetBit(PORTB, 2, 0);
+			PORTB = SetBit(PORTB, 3, 1);
 			//right
 			break;
 		case 'I':
+			PORTB = SetBit(PORTB, 0, 1);
+			PORTB = SetBit(PORTB, 1, 0);
+			PORTB = SetBit(PORTB, 2, 0);
+			PORTB = SetBit(PORTB, 3, 1);
 			//forward right 
 			break;
 		case 'J':
+			PORTB = SetBit(PORTB, 0, 0);
+			PORTB = SetBit(PORTB, 1, 1);
+			PORTB = SetBit(PORTB, 2, 1);
+			PORTB = SetBit(PORTB, 3, 0);
 			//back right
 			break;
 		default:
 			//forward
+			PORTB = SetBit(PORTB, 0, 1);
+			PORTB = SetBit(PORTB, 1, 0);
+			PORTB = SetBit(PORTB, 2, 1);
+			PORTB = SetBit(PORTB, 3, 0);
 			break;
 	}
 }
