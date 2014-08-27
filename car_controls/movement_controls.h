d9ecbeabc8a933c75041db7279c7a516a77011e0
cass-m-g <cass.m.garner@gmail.com>
2014-08-27 02:06:34 -0700

    Update movement_controls.h

--- car_controls/movement_controls.h
+++ car_controls/movement_controls.h
@@ -8,42 +8,76 @@
 //set max car speed***********
 double max_car_speed = 145;
 
-
-void set_car_speed(){
+void set_button_presses(){
+	button_press_movement = NULL;
+	button_press_speed = NULL;
+	
 	switch(button_press){
 		case '0':
-			
-			break;
 		case '1':
-			
-			break;
 		case '2':
-			
-			break;
 		case '3':
-			
-			break;
 		case '4':
-			
-			break;
 		case '5':
-			
-			break;
 		case '6':
-			
-			break;
 		case '7':
-			
-			break;
 		case '8':
-			
-			break;
 		case '9':
-			
-			break;
 		case 'q':
-			
+			button_press_speed = button_press;
 			break;
+		
+		case 'F':
+		case 'B':
+		case 'L':
+		case 'G':
+		case 'H':
+		case 'R':
+		case 'I':
+		case 'J':
+		case 'D':
+		case 'S':
+			button_press_movement = button_press;
+			break;
+	}
+}
+
+
+void set_car_speed(){
+	switch(button_press_speed){
+		case '0':
+		
+		break;
+		case '1':
+		
+		break;
+		case '2':
+		
+		break;
+		case '3':
+		
+		break;
+		case '4':
+		
+		break;
+		case '5':
+		
+		break;
+		case '6':
+		
+		break;
+		case '7':
+		
+		break;
+		case '8':
+		
+		break;
+		case '9':
+		
+		break;
+		case 'q':
+		
+		break;
 	}
 }
 
@@ -62,79 +96,79 @@ void set_car_stop(){
 void set_car_movement(){
 	switch(current_car_state){
 		case 'F':
-			//forward
-			PORTB = SetBit(PORTB, 0, 1);
-			PORTB = SetBit(PORTB, 1, 0);
-			PORTB = SetBit(PORTB, 2, 1);
-			PORTB = SetBit(PORTB, 3, 0);
-			
-			break;
+		//forward
+		PORTB = SetBit(PORTB, 0, 1);
+		PORTB = SetBit(PORTB, 1, 0);
+		PORTB = SetBit(PORTB, 2, 1);
+		PORTB = SetBit(PORTB, 3, 0);
+		
+		break;
 		case 'B':
-			PORTB = SetBit(PORTB, 0, 0);
-			PORTB = SetBit(PORTB, 1, 1);
-			PORTB = SetBit(PORTB, 2, 0);
-			PORTB = SetBit(PORTB, 3, 1);
-			//backward
-			break;
+		PORTB = SetBit(PORTB, 0, 0);
+		PORTB = SetBit(PORTB, 1, 1);
+		PORTB = SetBit(PORTB, 2, 0);
+		PORTB = SetBit(PORTB, 3, 1);
+		//backward
+		break;
 		case 'L':
-			PORTB = SetBit(PORTB, 0, 0);
-			PORTB = SetBit(PORTB, 1, 1);
-			PORTB = SetBit(PORTB, 2, 1);
-			PORTB = SetBit(PORTB, 3, 0);
-			//left
-			break;
+		PORTB = SetBit(PORTB, 0, 0);
+		PORTB = SetBit(PORTB, 1, 1);
+		PORTB = SetBit(PORTB, 2, 1);
+		PORTB = SetBit(PORTB, 3, 0);
+		//left
+		break;
 		case 'G':
-			PORTB = SetBit(PORTB, 0, 0);
-			PORTB = SetBit(PORTB, 1, 1);
-			PORTB = SetBit(PORTB, 2, 1);
-			PORTB = SetBit(PORTB, 3, 0);
-			//forward left
-			break;
+		PORTB = SetBit(PORTB, 0, 0);
+		PORTB = SetBit(PORTB, 1, 1);
+		PORTB = SetBit(PORTB, 2, 1);
+		PORTB = SetBit(PORTB, 3, 0);
+		//forward left
+		break;
 		case 'H':
-			PORTB = SetBit(PORTB, 0, 1);
-			PORTB = SetBit(PORTB, 1, 0);
-			PORTB = SetBit(PORTB, 2, 0);
-			PORTB = SetBit(PORTB, 3, 1);
-			//back left
-			break;
+		PORTB = SetBit(PORTB, 0, 1);
+		PORTB = SetBit(PORTB, 1, 0);
+		PORTB = SetBit(PORTB, 2, 0);
+		PORTB = SetBit(PORTB, 3, 1);
+		//back left
+		break;
 		case 'R':
-			PORTB = SetBit(PORTB, 0, 1);
-			PORTB = SetBit(PORTB, 1, 0);
-			PORTB = SetBit(PORTB, 2, 0);
-			PORTB = SetBit(PORTB, 3, 1);
-			//right
-			break;
+		PORTB = SetBit(PORTB, 0, 1);
+		PORTB = SetBit(PORTB, 1, 0);
+		PORTB = SetBit(PORTB, 2, 0);
+		PORTB = SetBit(PORTB, 3, 1);
+		//right
+		break;
 		case 'I':
-			PORTB = SetBit(PORTB, 0, 1);
-			PORTB = SetBit(PORTB, 1, 0);
-			PORTB = SetBit(PORTB, 2, 0);
-			PORTB = SetBit(PORTB, 3, 1);
-			//forward right 
-			break;
+		PORTB = SetBit(PORTB, 0, 1);
+		PORTB = SetBit(PORTB, 1, 0);
+		PORTB = SetBit(PORTB, 2, 0);
+		PORTB = SetBit(PORTB, 3, 1);
+		//forward right
+		break;
 		case 'J':
-			PORTB = SetBit(PORTB, 0, 0);
-			PORTB = SetBit(PORTB, 1, 1);
-			PORTB = SetBit(PORTB, 2, 1);
-			PORTB = SetBit(PORTB, 3, 0);
-			//back right
-			break;
+		PORTB = SetBit(PORTB, 0, 0);
+		PORTB = SetBit(PORTB, 1, 1);
+		PORTB = SetBit(PORTB, 2, 1);
+		PORTB = SetBit(PORTB, 3, 0);
+		//back right
+		break;
 		case 'D':
-			//stop all bluetooth disconected!!!
-			
-			PORTB = SetBit(PORTB, 0, 0);
-			PORTB = SetBit(PORTB, 1, 0);
-			PORTB = SetBit(PORTB, 2, 0);
-			PORTB = SetBit(PORTB, 3, 0);
-			
-			//manage emergency lights************
-			//emergency lights will flash on and off (250 ms)
-			//this can also be controlled by a button
-			
-			break;
+		//stop all bluetooth disconected!!!
+		
+		PORTB = SetBit(PORTB, 0, 0);
+		PORTB = SetBit(PORTB, 1, 0);
+		PORTB = SetBit(PORTB, 2, 0);
+		PORTB = SetBit(PORTB, 3, 0);
+		
+		//manage emergency lights************
+		//emergency lights will flash on and off (250 ms)
+		//this can also be controlled by a button
+		
+		break;
 		default:
-			//stop
-			set_car_stop();
-			break;
+		//stop
+		set_car_stop();
+		break;
 	}
 }
 
