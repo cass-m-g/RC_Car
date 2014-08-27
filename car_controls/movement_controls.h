8252d42d0c549d01cf9eeb848208afcd60452637
Cass Garner <cass.m.garner@gmail.com>
2014-08-27 12:36:16 -0700

    Update movement_controls.h

--- car_controls/movement_controls.h
+++ car_controls/movement_controls.h
@@ -6,7 +6,7 @@
 
 
 //set max car speed***********
-double max_car_speed = 145;
+double max_car_speed = 2.9; //this is in Hz
 
 void set_button_presses(){
 	button_press_movement = NULL;
@@ -84,10 +84,11 @@ void set_car_speed(){
 
 void set_car_stop(){
 	
-	PORTB = SetBit(PORTB, 0, 0);
-	PORTB = SetBit(PORTB, 1, 0);
-	PORTB = SetBit(PORTB, 2, 0);
-	PORTB = SetBit(PORTB, 3, 0);
+	//PORTB = SetBit(PORTB, 0, 0);
+	//PORTB = SetBit(PORTB, 1, 0);
+	//PORTB = SetBit(PORTB, 2, 0);
+	//PORTB = SetBit(PORTB, 3, 0);
+	set_PWM
 	
 	//turn on break lights**********
 	PORTA = SetBit(PORTA, 2, 1);
@@ -103,59 +104,60 @@ void set_car_movement(){
 	switch(current_car_state){
 		case 'F':
 		//forward
-		PORTB = SetBit(PORTB, 0, 1);
-		PORTB = SetBit(PORTB, 1, 0);
-		PORTB = SetBit(PORTB, 2, 1);
-		PORTB = SetBit(PORTB, 3, 0);
+		//PORTB = SetBit(PORTB, 0, 1);
+		//PORTB = SetBit(PORTB, 1, 0);
+		//PORTB = SetBit(PORTB, 2, 1);
+		//PORTB = SetBit(PORTB, 3, 0);
+		
 		
 		break;
 		case 'B':
-		PORTB = SetBit(PORTB, 0, 0);
-		PORTB = SetBit(PORTB, 1, 1);
-		PORTB = SetBit(PORTB, 2, 0);
-		PORTB = SetBit(PORTB, 3, 1);
+		//PORTB = SetBit(PORTB, 0, 0);
+		//PORTB = SetBit(PORTB, 1, 1);
+		//PORTB = SetBit(PORTB, 2, 0);
+		//PORTB = SetBit(PORTB, 3, 1);
 		//backward
 		break;
 		case 'L':
-		PORTB = SetBit(PORTB, 0, 0);
-		PORTB = SetBit(PORTB, 1, 1);
-		PORTB = SetBit(PORTB, 2, 1);
-		PORTB = SetBit(PORTB, 3, 0);
+		//PORTB = SetBit(PORTB, 0, 0);
+		//PORTB = SetBit(PORTB, 1, 1);
+		//PORTB = SetBit(PORTB, 2, 1);
+		//PORTB = SetBit(PORTB, 3, 0);
 		//left
 		break;
 		case 'G':
-		PORTB = SetBit(PORTB, 0, 0);
-		PORTB = SetBit(PORTB, 1, 1);
-		PORTB = SetBit(PORTB, 2, 1);
-		PORTB = SetBit(PORTB, 3, 0);
+		//PORTB = SetBit(PORTB, 0, 0);
+		//PORTB = SetBit(PORTB, 1, 1);
+		//PORTB = SetBit(PORTB, 2, 1);
+		//PORTB = SetBit(PORTB, 3, 0);
 		//forward left
 		break;
 		case 'H':
-		PORTB = SetBit(PORTB, 0, 1);
-		PORTB = SetBit(PORTB, 1, 0);
-		PORTB = SetBit(PORTB, 2, 0);
-		PORTB = SetBit(PORTB, 3, 1);
+		//PORTB = SetBit(PORTB, 0, 1);
+		//PORTB = SetBit(PORTB, 1, 0);
+		//PORTB = SetBit(PORTB, 2, 0);
+		//PORTB = SetBit(PORTB, 3, 1);
 		//back left
 		break;
 		case 'R':
-		PORTB = SetBit(PORTB, 0, 1);
-		PORTB = SetBit(PORTB, 1, 0);
-		PORTB = SetBit(PORTB, 2, 0);
-		PORTB = SetBit(PORTB, 3, 1);
+		//PORTB = SetBit(PORTB, 0, 1);
+		//PORTB = SetBit(PORTB, 1, 0);
+		//PORTB = SetBit(PORTB, 2, 0);
+		//PORTB = SetBit(PORTB, 3, 1);
 		//right
 		break;
 		case 'I':
-		PORTB = SetBit(PORTB, 0, 1);
-		PORTB = SetBit(PORTB, 1, 0);
-		PORTB = SetBit(PORTB, 2, 0);
-		PORTB = SetBit(PORTB, 3, 1);
+		//PORTB = SetBit(PORTB, 0, 1);
+		//PORTB = SetBit(PORTB, 1, 0);
+		//PORTB = SetBit(PORTB, 2, 0);
+		//PORTB = SetBit(PORTB, 3, 1);
 		//forward right
 		break;
 		case 'J':
-		PORTB = SetBit(PORTB, 0, 0);
-		PORTB = SetBit(PORTB, 1, 1);
-		PORTB = SetBit(PORTB, 2, 1);
-		PORTB = SetBit(PORTB, 3, 0);
+		//PORTB = SetBit(PORTB, 0, 0);
+		//PORTB = SetBit(PORTB, 1, 1);
+		//PORTB = SetBit(PORTB, 2, 1);
+		//PORTB = SetBit(PORTB, 3, 0);
 		//back right
 		break;
 		case 'D':
