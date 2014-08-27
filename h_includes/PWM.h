78aa2d609a2469eab39409490546a2262f23f48e
Cass Garner <cass.m.garner@gmail.com>
2014-08-27 12:14:09 -0700

    Update PWM.h

--- h_includes/PWM.h
+++ h_includes/PWM.h
@@ -1,11 +1,42 @@
+/*Replace OCR3A with:
+*  PORTB7 ---- OCR3B
+*  PORTB6 ---- written below
+*  PORTB4 ---- OC0B
+*  PORTB3 ---- OC0A
+*
+*/
+
 // 0.954 hz is lowest frequency possible with this function,
 // based on settings in PWM_on()
 // Passing in 0 as the frequency will stop the speaker from generating sound
-void set_PWM(double frequency) {
-	static double current_frequency; // Keeps track of the currently set frequency
+void set_PWM_PORTB7(double frequency) {
+	static double current_frequency_PORTB7; // Keeps track of the currently set frequency
 	// Will only update the registers when the frequency changes, otherwise allows
 	// music to play uninterrupted.
-	if (frequency != current_frequency) {
+	if (frequency != current_frequency_PORTB7) {
+		if (!frequency) { TCCR3B &= 0x08; } //stops timer/counter
+		else { TCCR3B |= 0x03; } // resumes/continues timer/counter
+		
+		// prevents OCR3B from overflowing, using prescaler 64
+		// 0.954 is smallest frequency that will not result in overflow
+		if (frequency < 0.954) { OCR3B = 0xFFFF; }
+		
+		// prevents OCR3B from underflowing, using prescaler 64					// 31250 is largest frequency that will not result in underflow
+		else if (frequency > 31250) { OCR3B = 0x0000; }
+		
+		// set OCR3B based on desired frequency
+		else { OCR3B = (short)(8000000 / (128 * frequency)) - 1; }
+
+		TCNT3 = 0; // resets counter
+		current_frequency_PORTB7 = frequency; // Updates the current frequency
+	}
+}
+
+void set_PWM_PORTB6(double frequency) {
+	static double current_frequency_PORTB6; // Keeps track of the currently set frequency
+	// Will only update the registers when the frequency changes, otherwise allows
+	// music to play uninterrupted.
+	if (frequency != current_frequency_PORTB6) {
 		if (!frequency) { TCCR3B &= 0x08; } //stops timer/counter
 		else { TCCR3B |= 0x03; } // resumes/continues timer/counter
 		
@@ -20,7 +51,53 @@ void set_PWM(double frequency) {
 		else { OCR3A = (short)(8000000 / (128 * frequency)) - 1; }
 
 		TCNT3 = 0; // resets counter
-		current_frequency = frequency; // Updates the current frequency
+		current_frequency_PORTB6 = frequency; // Updates the current frequency
+	}
+}
+
+void set_PWM_PORTB4(double frequency) {
+	static double current_frequency_PORTB4; // Keeps track of the currently set frequency
+	// Will only update the registers when the frequency changes, otherwise allows
+	// music to play uninterrupted.
+	if (frequency != current_frequency_PORTB4) {
+		if (!frequency) { TCCR3B &= 0x08; } //stops timer/counter
+		else { TCCR3B |= 0x03; } // resumes/continues timer/counter
+		
+		// prevents OC0B from overflowing, using prescaler 64
+		// 0.954 is smallest frequency that will not result in overflow
+		if (frequency < 0.954) { OC0B = 0xFFFF; }
+		
+		// prevents OC0B from underflowing, using prescaler 64					// 31250 is largest frequency that will not result in underflow
+		else if (frequency > 31250) { OC0B = 0x0000; }
+		
+		// set OC0B based on desired frequency
+		else { OC0B = (short)(8000000 / (128 * frequency)) - 1; }
+
+		TCNT3 = 0; // resets counter
+		current_frequency_PORTB4 = frequency; // Updates the current frequency
+	}
+}
+
+void set_PWM_PORTB3(double frequency) {
+	static double current_frequency_PORTB3; // Keeps track of the currently set frequency
+	// Will only update the registers when the frequency changes, otherwise allows
+	// music to play uninterrupted.
+	if (frequency != current_frequency_PORTB3) {
+		if (!frequency) { TCCR3B &= 0x08; } //stops timer/counter
+		else { TCCR3B |= 0x03; } // resumes/continues timer/counter
+		
+		// prevents OC0A from overflowing, using prescaler 64
+		// 0.954 is smallest frequency that will not result in overflow
+		if (frequency < 0.954) { OC0A = 0xFFFF; }
+		
+		// prevents OC0A from underflowing, using prescaler 64					// 31250 is largest frequency that will not result in underflow
+		else if (frequency > 31250) { OC0A = 0x0000; }
+		
+		// set OC0A based on desired frequency
+		else { OC0A = (short)(8000000 / (128 * frequency)) - 1; }
+
+		TCNT3 = 0; // resets counter
+		current_frequency_PORTB3 = frequency; // Updates the current frequency
 	}
 }
 
@@ -30,7 +107,10 @@ void PWM_on() {
 	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
 	// WGM32: When counter (TCNT3) matches OCR3A, reset counter
 	// CS31 & CS30: Set a prescaler of 64
-	set_PWM(0);
+	set_PWM_PORTB7(0);
+	set_PWM_PORTB6(0);
+	set_PWM_PORTB4(0);
+	set_PWM_PORTB3(0);
 }
 
 void PWM_off() {
